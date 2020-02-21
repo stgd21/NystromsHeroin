6a3d38379b94d56fa91b28ccb2df8996baed2860
stgd21 <45217325+stgd21@users.noreply.github.com>
2020-02-20 17:12:14 -0700

    Add files via upload

---
+++ Scripts/StandingPlayerState.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandingPlayerState : IPlayerState
+{
+    public void Enter(Player player)
+    {
+        Debug.Log("Entered Standing");
+        player.myCurrentState = this;
+        
+    }
+
+    public void Execute(Player player)
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            //transition to ducking
+            DuckingPlayerState duckingState = new DuckingPlayerState();
+            duckingState.Enter(player);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            JumpingPlayerState jumpingState = new JumpingPlayerState();
+            jumpingState.Enter(player);
+        }
+    }
+}
